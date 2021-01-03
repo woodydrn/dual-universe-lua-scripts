105a97bfacd9a0288fea118e9e978baa3f88bda3
Danny Asmussen <woodydrn@gmail.com>
2021-01-03 08:39:02 +0100

    Updated refiner mouse up

--- refiner-screen-mouseUp.lua
+++ refiner-screen-mouseUp.lua
@@ -1,5 +1,7 @@
 for i,area in pairs(clickableAreas) do
 	if (x >= area.x and x <= (area.x + area.hx) and y >= area.y and y <= (area.y + area.hy)) then
-        area.fun(area.refiner)
+		if (area.refiner ~= false) then area.fun(area.refiner)
+          else area.fun()
+		end
 	end
 end
