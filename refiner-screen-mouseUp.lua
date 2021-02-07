e2266eba480a05b777a776a87e7cfb17231f7289
Danny Asmussen <woodydrn@gmail.com>
2021-02-07 20:00:48 +0100

    Added parameters

--- refiner-screen-mouseUp.lua
+++ refiner-screen-mouseUp.lua
@@ -1,6 +1,6 @@
-for i,area in pairs(clickableAreas) do
+for i, area in pairs(clickableAreas) do
 	if (x >= area.x and x <= (area.x + area.hx) and y >= area.y and y <= (area.y + area.hy)) then
-		if (area.refiner ~= false) then area.fun(area.refiner)
+		if (area.parameter ~= false) then area.fun(area.parameter)
           else area.fun()
 		end
 	end
